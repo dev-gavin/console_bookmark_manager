2515f07cb49a06702c458f12d90e51c75b260484
Gavin Meeker <gavin@Gavins-Laptop.local>
2024-09-05 06:48:05 -0400

    implement basic input handling

--- Program.cs
+++ Program.cs
@@ -1,9 +1,43 @@
 ﻿namespace bookmark_manager;
 
+// using Microsoft.Extensions.DependencyInjection;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Clear();
+        Console.WriteLine("Welcome to the Command-Line Bookmark Manager!");
+        Console.WriteLine("Type 'help' for a list of commands.");
+
+        bool isValidInput = false;
+        while (!isValidInput)
+        {
+            var input = GetUserInput();
+            var inputArray = input.Split(" ");
+            isValidInput = IsValidInput(inputArray);
+        }
+    }
+
+    private static bool IsValidInput(string[] inputArray)
+    {
+        bool isValidInput = false;
+        string[] validCommands = { "add", "list", "search", "edit", "export", "exit" };
+        isValidInput = validCommands.Contains(inputArray[0]);
+
+        return isValidInput;
+    }
+
+    private static string GetUserInput()
+    {
+        Console.Write(Environment.NewLine);
+        Console.Write("> ");
+        string? input = Console.ReadLine();
+        while (string.IsNullOrEmpty(input))
+        {
+            Console.Write("> ");
+            input = Console.ReadLine();
+        }
+        return input;
     }
 }
