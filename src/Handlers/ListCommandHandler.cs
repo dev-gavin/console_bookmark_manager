f84a35a67dee0ba1fac8bb7f58a8f8e03ec3addf
Gavin Meeker <gavin@Gavins-Laptop.local>
2024-09-15 09:01:20 -0400

    refactor listcommandhandler

--- src/Handlers/ListCommandHandler.cs
+++ src/Handlers/ListCommandHandler.cs
@@ -7,17 +7,24 @@ public class ListCommandHandler : ICommandHandler
 {
     public void HandleCommand(string[] arguments, BookmarkManagerContext context)
     {
-        var bookmarks =
+        // TODO: need better handling of command flags/arguments
+        bool showArchived = arguments.Length > 0 && arguments[0].Equals("--archived");
+
+        IQueryable<Bookmark> bookmarks;
+        bookmarks =
             from bookmark in context.Bookmarks
-            where bookmark.IsArchived == false
+            where bookmark.IsArchived == showArchived
             select bookmark;
 
         foreach (Bookmark bookmark in bookmarks)
         {
             var tags = from tag in context.Tags where tag.Bookmark.Id == bookmark.Id select tag;
-            var formattedTags = string.Join(",", tags.Select(tag => tag.Name));
+            var formattedTags = tags.Any()
+                ? string.Join(",", tags.Select(tag => tag.Name))
+                : "No Tags";
+
             Console.WriteLine(
-                $"[{bookmark.Id}] {bookmark.Name} - {bookmark.Url} (Tags: {formattedTags})"
+                $"{(showArchived ? "ARCHIVED: " : "")}[{bookmark.Id}] {bookmark.Name} - {bookmark.Url} (Tags: {formattedTags})"
             );
         }
     }
